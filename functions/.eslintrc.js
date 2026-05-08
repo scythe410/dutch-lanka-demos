7f1799fa358d7aa3836b533d8fb55c097d27a003
scythe410 <ramirumc@gmail.com>
2026-05-09 03:46:26 +0530

    chore(functions): include eslint config and package .gitignore
    Missed in the original Cloud Functions scaffold commit.

---
+++ functions/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*",
+    "/generated/**/*",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "object-curly-spacing": ["error", "always"],
+    "max-len": ["warn", { "code": 120 }],
+  },
+  overrides: [
+    {
+      files: ["test/**/*.ts"],
+      rules: {
+        // Tests use lots of small inline helper functions; JSDoc on each
+        // is noise.
+        "require-jsdoc": "off",
+        "valid-jsdoc": "off",
+      },
+    },
+  ],
+};
